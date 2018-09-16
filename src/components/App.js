3b3b04dbafe62fa236fe9277aea3816d17bb6e8d
James Johnson <johnsojr@gmail.com>
2018-09-16 16:09:52 -0500

    Update app name

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import TodoList from './TodoList';
 export default ({ todos }) => (
   <div id="app">
     <header class="app-bar">
-      <h1>Todos</h1>
+      <h1>HyperApp To Do</h1>
     </header>
     <section class="hero-section">
       <NewTodoInput placeholder="What do you need to do?" />
     </section>
     <section class="page-section">
-      <h2 class="section-heading">My List</h2>
+      <h2 class="section-heading">To Do</h2>
       <TodoList todos={todos} heading="My List" />
       <div class="filter-section">
         <ul>
